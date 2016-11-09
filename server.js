8415f91122cfbed3aad5271c322494ae4afb2a87
Firifon <fromeromurat@gmail.com>
2016-11-09 08:09:50 +0000

    public folder

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express')
+var moment = require("moment")
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/:time', function (req, res) {
+    var time = req.params.time;
+    var unix;
+    var natural;
+    
+    if (isNaN(time)) {
+        unix = moment(time).unix();
+        natural = time;
+    } else {
+        unix = time;
+        natural = moment.unix(time).format("MMM DD, YYYY");
+    }
+    
+    var date = { "unix": unix, "natural": natural };
+    res.send(date);
+    
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
